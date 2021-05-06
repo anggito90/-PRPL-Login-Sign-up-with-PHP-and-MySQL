5a728219caa23a429bafe8f4da7b05cbf98279f6
Anggito <62731143+anggito90@users.noreply.github.com>
2021-05-06 21:23:05 +0700

    Code description update

--- database.php
+++ database.php
@@ -11,13 +11,13 @@ $db = mysqli_connect('localhost', 'root', '', 'prplregistration');
 
 // REGISTER USER
 if (isset($_POST['reg_user'])) {
-  
+  // receiving all input values from the form
   $username = mysqli_real_escape_string($db, $_POST['username']);
   $email = mysqli_real_escape_string($db, $_POST['email']);
   $password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
   $password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
 
-  //error pop up 
+  //form validation, if there's an empty box, a message will pop up in the box by adding (array_push()) corresponding error unto $errors array
   if (empty($username)) { array_push($errors, "Username can not be empty!"); }
   if (empty($email)) { array_push($errors, "Email can not be empty!"); }
   if (empty($password_1)) { array_push($errors, "Password can not be empty!"); }
@@ -25,14 +25,14 @@ if (isset($_POST['reg_user'])) {
 	array_push($errors, "Password does not match");
   }
 
-  //checking if there is already the same user in database
+  //checking if there is already the same user or email in the database
   $user_check_query = "SELECT * FROM users WHERE username='$username' OR email='$email' LIMIT 1";
   $result = mysqli_query($db, $user_check_query);
   $user = mysqli_fetch_assoc($result);
   
-  if ($user) { // if user exists
+  if ($user) { // if exists
     if ($user['username'] === $username) {
-      array_push($errors, "Username already exists");
+      array_push($errors, "Username already exists"); // display message
     }
 
     if ($user['email'] === $email) {
@@ -40,7 +40,7 @@ if (isset($_POST['reg_user'])) {
     }
   }
 
-
+//if there's no error, register the user
   if (count($errors) == 0) {
   	$password = md5($password_1); //password encryption, to make it more secure
 
@@ -59,10 +59,10 @@ if (isset($_POST['login_user'])) {
   $password = mysqli_real_escape_string($db, $_POST['password']);
 
   if (empty($username)) {
-  	array_push($errors, "Username can not be empty!");
+  	array_push($errors, "Username can not be empty!"); //if the username is empty, will display this pop up
   }
   if (empty($password)) {
-  	array_push($errors, "Password can not be empty!");
+  	array_push($errors, "Password can not be empty!"); //if the password is empty, will display this pop up
   }
 
   if (count($errors) == 0) {
